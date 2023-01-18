58e99da2f86e150d3b5bdca9b452b1ec18d76fec
ygwhite <ru516@yandex.ru>
2023-01-19 00:49:37 +0300

    Добавил лайки, рейтинг

--- my_project/store/admin.py
+++ my_project/store/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from .models import Clothes
+from .models import Clothes, UserClotheRelation
 
 
 @admin.register(Clothes)
 class ClothesAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(UserClotheRelation)
+class UserRelationAdmin(ModelAdmin):
     pass
--- my_project/store/models.py
+++ my_project/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -9,3 +10,19 @@ class Clothes(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserClotheRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ужасно!'),
+        (2, 'Плохо!'),
+        (3, 'Неплохо!'),
+        (4, 'Отлично!'),
+        (5, 'Я безумно доволен покупкой!')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    clothe = models.ForeignKey(Clothes, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_favorites = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
